6b194ff5e0583e6ee2f39fac438a3732ba56e307
paul valencia <apvalencia92@gmail.com>
2025-03-12 14:35:42 -0500

    Instalación y configuración de material UI e implementación de theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { AppTheme } from './theme/AppTheme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppTheme>
+      <App />
+    </AppTheme>
   </StrictMode>,
 )
